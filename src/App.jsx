c6e57bd8469bf21c5636ebc0517cb5af418cb5c8
Avinash sharma <sharmaavi2196@gmail.com>
2025-01-18 04:53:19 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
+      </div>
+    </div>
+  );
+};
+
+const FriendList = () => {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+};
+
+const Friend = ({ friend }) => {
+  const { id, image, balance } = friend;
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${friend.balance}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && <p>you and {friend.name} are even</p>}
+      <Button>Select</Button>
+    </li>
+  );
+};
+
+const Button = ({ children }) => {
+  return <button className="button">{children}</button>;
+};
+
+const FormAddFriend = () => {
+  return (
+    <form className="form-add-friend">
+      <label>🍟friend name</label>
+      <input type="text" />
+
+      <label>🌆 Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+};
+
+export default App;
